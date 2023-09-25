c64b711781ff13ab66ce52f9bb5a18bf248ee42e
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2023-09-25 15:25:22 +0100

    add sort algorithms

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,36 +1,44 @@
 def bubble_sort(arr: list) -> None:
-    for i in range(len(arr) - 1):
-        for j in range(i + 1, len(arr)):
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+    for i in range(len(arr) - 2, -1, -1):
+        for j in range(i + 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+def insertion_sort(arr: list) -> None:
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j - 1] > arr[j]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j -= 1
 
 
 def quick_sort(arr: list) -> None:
-    def impl(arr: list, l: int, r: int):
-        if l >= r:
-            return
+    def partition(L: list, low: int, high: int) -> int:
+        pivot = L[low]
+        i = low - 1
+        j = high + 1
+        while True:
+            i += 1
+            while L[i] < pivot:
+                i += 1
+            j -= 1
+            while L[j] > pivot:
+                j -= 1
+            if i >= j:
+                return j
+            L[i], L[j] = L[j], L[i]
 
-        pivot = arr[l]
-        L, R = l, r
-        while l < r:
-            while l < r and arr[r] >= pivot:
-                r -= 1
-            if l < r:
-                arr[l] = arr[r]
-            while l < r and arr[l] < pivot:
-                l += 1
-            if l < r:
-                arr[r] = arr[l]
-        if l != L:
-            arr[l] = pivot
-
-        impl(arr, L, l - 1)
-        impl(arr, l + 1, R)
+    def impl(L: list, low: int, high: int) -> None:
+        if low < high:
+            p = partition(L, low, high)
+            impl(L, low, p)
+            impl(L, p + 1, high)
 
     impl(arr, 0, len(arr) - 1)
 
 
-def LomutoQS(L: list) -> None:
+def LomutoQS(arr: list) -> None:
     def Lomuto_partition(L: list, lo: int, hi: int) -> int:
         pivot = L[hi]
         i = lo
@@ -47,7 +55,27 @@ def LomutoQS(L: list) -> None:
             impl(L, lo, p - 1)
             impl(L, p + 1, hi)
 
-    impl(L, 0, len(L) - 1)
+    impl(arr, 0, len(arr) - 1)
+
+
+def heap_sort(arr: list) -> None:
+    def push_down(i: int, n: int) -> None:
+        k = i
+        while 2 * k + 1 < n:
+            j = 2 * k + 1
+            if j + 1 < n and arr[j] < arr[j + 1]:
+                j += 1
+            if arr[k] < arr[j]:
+                arr[k], arr[j] = arr[j], arr[k]
+                k = j
+            else:
+                break
+
+    for i in range(len(arr) // 2, -1, -1):
+        push_down(i, len(arr))
+    for i in range(len(arr) - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        push_down(0, i)
 
 
 def merge_sort(arr: list) -> None:
@@ -85,7 +113,9 @@ def merge_sort(arr: list) -> None:
 
 sorting_algorithms = [
     ("bubble sort", bubble_sort),
+    ("insertion sort", insertion_sort),
     ("quick sort", quick_sort),
     ("Lomuto quick sort", LomutoQS),
+    ("heap sort", heap_sort),
     ("merge sort", merge_sort),
 ]
