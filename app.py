aa869c5dd5251aef4ca2f9e172da394bddc3659b
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2023-10-20 20:17:37 +0100

    refactor on_data; move show_statistics to on_data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import inspect
+from dataclasses import dataclass, field, fields
 from typing import Optional
 
 import dash_bootstrap_components as dbc
@@ -6,8 +7,8 @@ import dash_cytoscape as cyto
 import dash_mantine_components as dmc
 import numpy as np
 import plotly.express as px
+import plotly.graph_objs as go
 from dash import Dash, Input, Output, State, callback, ctx, dash_table, dcc, html
-from dash.exceptions import PreventUpdate
 from dash_iconify import DashIconify
 from flask_executor import Executor
 
@@ -16,32 +17,48 @@ from Elements import Elements
 from sorting_algorithms import sorting_algorithms
 
 
+@dataclass
+class OnDataCallbackOutput:
+    progress__value: int = 0
+    progress__max: int = 1
+    progress__animated: bool = False
+    progress__label: str = ""
+    progress_interval__disabled: bool = True
+    show_statistics__disabled: bool = True
+    expand_all__disabled: bool = True
+    reset__disabled: bool = True
+    visiblity_state__data: Optional[str] = None
+    cytoscape__elements: list[dict] = field(default_factory=list)
+    notifications_container__children: list | dmc.Notification = field(default_factory=list)
+    buffered_input__data: Optional[list] = None
+    last_tree_param__data: Optional[list] = None
+    statistics_modal__is_open: bool = False
+    statistics_graph__figure: dict | go.Figure = field(default_factory=dict)
+    statistics_table__data: list = field(default_factory=list)
+    control_loading__style: dict = field(default_factory=dict)
+    control_loading_output__children: list = field(default_factory=list)
+
+    @classmethod
+    def outputs(cls) -> list[Output]:
+        return [Output(*field.name.split("__")) for field in fields(cls)]
+
+    def to_list(self) -> list:
+        return [getattr(self, field.name) for field in fields(self)]
+
+
 @callback(
-    Output("progress", "value"),
-    Output("progress", "max"),
-    Output("progress", "animated"),
-    Output("progress", "label"),
-    Output("progress-interval", "disabled"),
-    Output("show-statistics", "disabled"),
-    Output("expand-all", "disabled"),
-    Output("reset", "disabled"),
-    Output("visiblity-state", "data"),
-    Output("cytoscape", "elements"),
-    Output("notifications-container", "children"),
-    Output("control-loading", "style"),
-    Output("control-loading-output", "children"),
-    Output("buffered-input", "data"),
-    Output("last-tree-param", "data"),
-    Input("visiblity-state", "data"),
-    Input("sorting-algorithm", "value"),
-    Input("input-N", "value"),
-    Input("show-full-labels", "value"),
+    OnDataCallbackOutput.outputs(),
+    Input("visiblity_state", "data"),
+    Input("sorting_algorithm", "value"),
+    Input("input_N", "value"),
+    Input("show_full_labels", "value"),
     Input("cytoscape", "tapNode"),
-    Input("expand-all", "n_clicks"),
+    Input("expand_all", "n_clicks"),
     Input("reset", "n_clicks"),
-    Input("progress-interval", "n_intervals"),
-    State("buffered-input", "data"),
-    State("last-tree-param", "data"),
+    Input("show_statistics", "n_clicks"),
+    Input("progress_interval", "n_intervals"),
+    State("buffered_input", "data"),
+    State("last_tree_param", "data"),
 )
 def on_data(
     visiblity_state: Optional[str],
@@ -51,19 +68,19 @@ def on_data(
     node: dict,
     _expand_all: int,
     _reset: int,
+    _show_statistics: int,
     _n_intervals: int,
     buffered_input: Optional[list],
     last_tree_param: Optional[list],
 ):
-    tree_param = [int(sorting_algorithm_i), int(input_N)]
+    ret = OnDataCallbackOutput()
     if input_N is None:
-        return [0, 1, False, "", True, True, True, True, None, [], [], {}, "", None, tree_param]
-
-    notification = []
-    if tree_param != last_tree_param and sorting_algorithms[int(sorting_algorithm_i)].max_N < int(input_N):
+        return ret.to_list()
+    ret.last_tree_param__data = [int(sorting_algorithm_i), int(input_N)]
+    if ret.last_tree_param__data != last_tree_param and sorting_algorithms[int(sorting_algorithm_i)].max_N < int(input_N):
         sorting_algorithm = sorting_algorithms[int(sorting_algorithm_i)]
-        notification = dmc.Notification(
-            id="warning-notification",
+        ret.notifications_container__children = dmc.Notification(
+            id="warning_notification",
             title="Warning",
             action="show",
             message=f"Input `N={input_N}` is too large (upper limit for `{sorting_algorithm.name}` is {sorting_algorithm.max_N}), "
@@ -73,56 +90,70 @@ def on_data(
         )
 
     trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-    if trigger_id in ("sorting-algorithm", "input-N", "reset"):
+    if trigger_id in ("sorting_algorithm", "input_N", "reset"):
         visiblity_state = None
 
     element_holder = Elements.get_element_holder(int(sorting_algorithm_i), int(input_N))
-    i, total = element_holder.get_progress()
+    ret.progress__value, ret.progress__max = i, total = element_holder.get_progress()
+    ret.progress__label = f"{i}/{total}"
     if i != total:
         if not element_holder.get_and_set_initialize_scheduled():
             executor.submit(element_holder.initialize, int(sorting_algorithm_i), int(input_N))
         if trigger_id == "cytoscape":
-            buffered_input = ["tap", int(node["data"]["id"])]
-        elif trigger_id == "expand-all":
-            buffered_input = ["expand-all"]
-        return [i, total, True, f"{i}/{total}", False, True, True, True, visiblity_state, [], notification, {"visibility": "hidden"}, "", buffered_input, tree_param]
+            ret.buffered_input__data = ["tap", int(node["data"]["id"])]
+        elif trigger_id == "expand_all":
+            ret.buffered_input__data = ["expand_all"]
+        elif trigger_id == "show_statistics":
+            ret.buffered_input__data = ["show_statistics"]
+        else:
+            ret.buffered_input__data = buffered_input
+        ret.progress__animated = True
+        ret.progress_interval__disabled = False
+        ret.control_loading__style = {"visibility": "hidden"}
+        return ret.to_list()
+    ret.show_statistics__disabled = False
+    ret.expand_all__disabled = False
+    ret.reset__disabled = False
 
     element_holder.wait_until_initialized()
     elements = Elements(element_holder, visiblity_state)
-    if trigger_id == "cytoscape" or (buffered_input is not None and buffered_input[0] == "tap"):
+    if trigger_id == "show_statistics" or (buffered_input is not None and buffered_input[0] == "show_statistics"):
+        data = np.array(element_holder.operation_cnts, dtype=np.int32)
+        ret.statistics_graph__figure = px.histogram(x=data, title="Operation Count Distribution", labels={"x": "Operation Count"}, color=data, text_auto=True)
+        ret.statistics_graph__figure.layout.update(showlegend=False)
+        ret.statistics_modal__is_open = True
+        ret.statistics_table__data = [{"Best": data.min(), "Worst": data.max(), "Average": f"{data.mean():.2f}"}]
+    elif trigger_id == "cytoscape" or (buffered_input is not None and buffered_input[0] == "tap"):
         elements.on_tap_node(int(node["data"]["id"]) if trigger_id == "cytoscape" else buffered_input[1])
-    elif trigger_id == "expand-all" or (buffered_input is not None and buffered_input[0] == "expand-all"):
+    elif trigger_id == "expand_all" or (buffered_input is not None and buffered_input[0] == "expand_all"):
         if not elements.expand_all():
-            notification = dmc.Notification(
-                id="warning-notification",
+            ret.notifications_container__children = dmc.Notification(
+                id="warning_notification",
                 title="Warning",
                 action="show",
                 message=f"Too many elements, only {MAX_ELEMENTS} elements are displayed.",
                 icon=DashIconify(icon="material-symbols:warning"),
                 autoClose=10000,
             )
-    visiblity_state = elements.get_visiblity_state()
-    visible_elements = elements.visible_elements(bool(show_full_labels))
-    return [i, total, False, f"{i}/{total}", True, False, False, False, visiblity_state, visible_elements, notification, {}, "", None, tree_param]
+    ret.visiblity_state__data = elements.get_visiblity_state()
+    ret.cytoscape__elements = elements.visible_elements(bool(show_full_labels))
+    return ret.to_list()
 
 
-@callback(Output("input-N", "invalid"), Input("input-N", "value"))
+@callback(Output("input_N", "invalid"), Input("input_N", "value"))
 def on_input_N_invalid(input_N: Optional[str]):
     return input_N is None
 
 
 @callback(
-    Output("code-modal", "is_open"),
-    Output("code-modal", "children"),
-    Output("control-loading-output", "children", allow_duplicate=True),
-    Input("show-code", "n_clicks"),
-    State("code-modal", "is_open"),
-    State("sorting-algorithm", "value"),
+    Output("code_modal", "is_open"),
+    Output("code_modal", "children"),
+    Output("control_loading_output", "children", allow_duplicate=True),
+    Input("show_code", "n_clicks"),
+    State("sorting_algorithm", "value"),
     prevent_initial_call=True,
 )
-def on_show_code(_: int, is_open: bool, sorting_algorithm_i: str):
-    if is_open:
-        return [False, []]
+def on_show_code(_show_code: int, sorting_algorithm_i: str):
     sorting_algo = sorting_algorithms[int(sorting_algorithm_i)]
     code = inspect.getsource(sorting_algo.func).strip()
     children = [
@@ -132,40 +163,16 @@ def on_show_code(_: int, is_open: bool, sorting_algorithm_i: str):
     return [True, children, ""]
 
 
-@callback(
-    Output("statistics-modal", "is_open"),
-    Output("statistics-graph", "figure"),
-    Output("statistics-table", "data"),
-    Output("control-loading-output", "children", allow_duplicate=True),
-    Input("show-statistics", "n_clicks"),
-    State("statistics-modal", "is_open"),
-    State("sorting-algorithm", "value"),
-    State("input-N", "value"),
-    prevent_initial_call=True,
-)
-def on_show_statics(_: int, is_open: bool, sorting_algorithm_i: str, input_N: Optional[str]):
-    if is_open:
-        return [False, {}, []]
-    if input_N is None:
-        raise PreventUpdate
-    element_holder = Elements.get_element_holder(int(sorting_algorithm_i), int(input_N))
-    element_holder.wait_until_initialized()
-    data = np.array(element_holder.operation_cnts, dtype=np.int32)
-    fig = px.histogram(x=data, title="Operation Count Distribution", labels={"x": "Operation Count"}, color=data, text_auto=True)
-    fig.layout.update(showlegend=False)
-    return [True, fig, [{"Best": data.min(), "Worst": data.max(), "Average": f"{data.mean():.2f}"}], ""]
-
-
 control_panel = html.Div(
     [
-        dbc.Button("Expand All", id="expand-all", disabled=True),
+        dbc.Button("Expand All", id="expand_all", disabled=True),
         dbc.Button("Reset", id="reset", disabled=True),
         dbc.Row(
             [
                 "Sorting Algorithm:",
                 dbc.Select(
                     options=[{"label": sorting_algo.name, "value": i} for i, sorting_algo in enumerate(sorting_algorithms)],
-                    id="sorting-algorithm",
+                    id="sorting_algorithm",
                     style={"width": "12rem"},
                     value=str(SORTING_ALGORITHM_I),
                     persistence=True,
@@ -174,12 +181,12 @@ control_panel = html.Div(
             ],
             style={"column-gap": "0", "display": "flex", "align-items": "center", "padding": "0.5rem"},
         ),
-        dbc.Button("Show Code", id="show-code"),
+        dbc.Button("Show Code", id="show_code"),
         dbc.Row(
             [
                 f"N(>0):",
                 dbc.Input(
-                    id="input-N",
+                    id="input_N",
                     type="number",
                     min=1,
                     step=1,
@@ -193,18 +200,18 @@ control_panel = html.Div(
             style={"column-gap": "0", "display": "flex", "align-items": "center", "padding": "0.5rem"},
         ),
         dbc.Progress(id="progress", value=0, striped=True, animated=True, style={"width": "10rem", "height": "1.3rem"}),
-        dcc.Interval(id="progress-interval", interval=300, n_intervals=0, disabled=True),
-        dbc.Button("Show Statistics", id="show-statistics", disabled=True),
+        dcc.Interval(id="progress_interval", interval=300, n_intervals=0, disabled=True),
+        dbc.Button("Show Statistics", id="show_statistics", disabled=True),
         dbc.Checklist(  # don't know why dbc.Switch cannot align center vertically, so use dbc.Checklist instead
             options=[{"label": "Show Full Labels", "value": 0}],
-            id="show-full-labels",
+            id="show_full_labels",
             value=[0] if SHOW_FULL_LABELS else [],
             switch=True,
             inline=True,
             persistence=True,
             persistence_type=USER_STATE_STORAGE_TYPE,
         ),
-        dcc.Loading(id="control-loading", type="default", children=html.Div(id="control-loading-output"), style={"visibility": "hidden"}),
+        dcc.Loading(id="control_loading", type="default", children=html.Div(id="control_loading_output"), style={"visibility": "hidden"}),
     ],
     style={"column-gap": "1rem", "display": "flex", "align-items": "center", "margin": "1rem", "flex-wrap": "wrap"},
 )
@@ -228,9 +235,9 @@ cytoscape = cyto.Cytoscape(
     ],
     autoRefreshLayout=True,
 )
-code_modal = dbc.Modal(id="code-modal", is_open=False, scrollable=True)
+code_modal = dbc.Modal(id="code_modal", is_open=False, scrollable=True)
 statistics_modal = dbc.Modal(
-    id="statistics-modal",
+    id="statistics_modal",
     size="xl",
     is_open=False,
     scrollable=True,
@@ -238,9 +245,9 @@ statistics_modal = dbc.Modal(
         dbc.ModalHeader(dbc.ModalTitle("Statistics")),
         dbc.ModalBody(
             [
-                dcc.Graph(id="statistics-graph"),
+                dcc.Graph(id="statistics_graph"),
                 dash_table.DataTable(
-                    id="statistics-table",
+                    id="statistics_table",
                     style_cell={"textAlign": "center"},
                     columns=[{"name": x, "id": x} for x in ("Best", "Worst", "Average")],
                 ),
@@ -248,9 +255,9 @@ statistics_modal = dbc.Modal(
         ),
     ],
 )
-visiblity_state = dcc.Store(id="visiblity-state", storage_type=USER_STATE_STORAGE_TYPE)
-buffered_input = dcc.Store(id="buffered-input", storage_type=USER_STATE_STORAGE_TYPE)
-last_tree_param = dcc.Store(id="last-tree-param", storage_type=USER_STATE_STORAGE_TYPE)
+visiblity_state = dcc.Store(id="visiblity_state", storage_type=USER_STATE_STORAGE_TYPE)
+buffered_input = dcc.Store(id="buffered_input", storage_type=USER_STATE_STORAGE_TYPE)
+last_tree_param = dcc.Store(id="last_tree_param", storage_type=USER_STATE_STORAGE_TYPE)
 
 app = Dash(
     __name__,
@@ -266,7 +273,7 @@ app = Dash(
 )
 app.layout = dmc.NotificationsProvider(
     html.Div(
-        [html.Div(id="notifications-container"), control_panel, cytoscape, code_modal, statistics_modal, visiblity_state, buffered_input, last_tree_param],
+        [html.Div(id="notifications_container"), control_panel, cytoscape, code_modal, statistics_modal, visiblity_state, buffered_input, last_tree_param],
         style={"height": "90vh", "width": "98vw", "margin": "auto"},
     )
 )
