84310e82168e137fb8232bddb7da8cd5405beeaf
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2023-10-28 09:20:20 +0100

    copy back after merging, prevent display glitch

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -116,26 +116,20 @@ def heap_sort(arr: list) -> None:
 
 def merge_sort(arr: list) -> None:
     def merge(arr: list, l: int, m: int, r: int) -> None:
-        L = arr[l : m + 1]
-        R = arr[m + 1 : r + 1]
-        i = j = 0
-        k = l
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
+        result = []
+        i, j = l, m + 1
+        while i <= m and j <= r:
+            if arr[i] < arr[j]:
+                result.append(arr[i])
                 i += 1
             else:
-                arr[k] = R[j]
+                result.append(arr[j])
                 j += 1
-            k += 1
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
+        for i in range(i, m + 1):
+            result.append(arr[i])
+        for j in range(j, r + 1):
+            result.append(arr[j])
+        arr[l : r + 1] = result
 
     def impl(arr: list, l: int, r: int) -> None:
         if l < r:
