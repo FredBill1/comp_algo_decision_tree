5dbed24e78de63def2eda4a3a1e8c0ca89340878
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2023-11-08 08:58:06 +0000

    bubble_max to find_max

--- cmp_algorithms/impl/find_max.py
+++ cmp_algorithms/impl/find_max.py
@@ -4,22 +4,22 @@ from math import factorial
 from ..CmpAlgorithm import CmpAlgorithm
 
 
-def bubble_max(arr: list) -> None:
-    for i in range(len(arr) - 1):
-        if arr[i] > arr[i + 1]:
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+def find_max(arr: list) -> None:
+    for i in range(1, len(arr)):
+        if arr[i] > arr[0]:
+            arr[0], arr[i] = arr[i], arr[0]
 
 
 def _validator(arr: Iterable[int]) -> bool:
     arr = list(arr)
-    if arr[-1] != len(arr) - 1:
+    if arr[0] != len(arr) - 1:
         return False
     return all(i == v for i, v in enumerate(sorted(arr)))
 
 
 algorithm = CmpAlgorithm(
-    "bubble max",
-    bubble_max,
+    "find max",
+    find_max,
     9,
     output_total=lambda N: factorial(N - 1),
     validator=_validator,
