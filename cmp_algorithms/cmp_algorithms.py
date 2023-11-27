1303e8e467d3aac2cbada73e1876b9cc86019367
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2023-11-27 16:21:28 +0000

    sort algorithms by path

--- cmp_algorithms/cmp_algorithms.py
+++ cmp_algorithms/cmp_algorithms.py
@@ -4,6 +4,6 @@ from pathlib import Path
 from .CmpAlgorithm import CmpAlgorithm
 
 cmp_algorithms: list[CmpAlgorithm] = []
-for file in (Path(__file__).parent / "impl").glob("*.py"):
+for file in sorted((Path(__file__).parent / "impl").glob("*.py")):
     module = import_module(f".{file.stem}", package="cmp_algorithms.impl")
     cmp_algorithms.append(module.algorithm)
