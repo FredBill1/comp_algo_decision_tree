20bd02703980688d284789783546f093f015c67f
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2024-01-17 23:13:06 +0000

    fix total number of partitions

--- cmp_algorithms/impl/lomuto_partition.py
+++ cmp_algorithms/impl/lomuto_partition.py
@@ -1,3 +1,5 @@
+from threading import Lock
+
 from ..CmpAlgorithm import CmpAlgorithm
 
 
@@ -13,11 +15,36 @@ def lomuto_partition(L: list) -> int:
     return i
 
 
+class A356291:
+    "https://oeis.org/A356291"
+
+    def __init__(self):
+        self.F = 1
+        self.R = [0]
+        self.C = [1]
+        self._lock = Lock()
+
+    def __call__(self, i: int) -> int:
+        with self._lock:
+            if i < len(self.R):
+                return self.R[i]
+            self.C += [0] * (i - len(self.C) + 1)
+            for n in range(len(self.R), i + 1):
+                self.F *= n
+                for k in range(n, 0, -1):
+                    self.C[k] = self.C[k - 1] * k
+                self.C[0] = -sum(self.C[k] for k in range(1, n + 1))
+                self.R.append(self.F + self.C[0])
+            return self.R[i]
+
+
+_a356291 = A356291()
+
+
 def output_total(N: int) -> int:
-    fact = [1] * (N)
-    for i in range(1, N):
-        fact[i] = fact[i - 1] * i
-    return sum(a * b for a, b in zip(fact, reversed(fact)))
+    if N <= 1:
+        return 1
+    return _a356291(N)
 
 
 def validator(L: list, i: int) -> bool:
