0ca0c172a480b04b6f1b4fdd62bef193fc8fb4fb
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2024-04-10 16:54:15 +0100

    label not updating is actually a bug from dash-cytoscape==1.0.0

--- comp_algo_decision_tree/decision_tree_gen/Nodes.py
+++ comp_algo_decision_tree/decision_tree_gen/Nodes.py
@@ -188,10 +188,8 @@ class Nodes:
                 classes.append("is_leaf")
             elif self.node_has_hidden_child(node):
                 classes.append("has_hidden_child")
-            if show_full_labels:
-                classes.append("full_label")
             label = self.node_holder.cmp_algorithm.get_label(node, self.node_holder.idx_use_letter, LABEL_MAX_LENGTH if show_full_labels else LABEL_CROP_LENGTH)
-            node_data = {"data": {"id": str(node.id), ("full_label" if show_full_labels else "label"): label}, "classes": " ".join(classes)}
+            node_data = {"data": {"id": str(node.id), "label": label}, "classes": " ".join(classes)}
             ret.append(node_data)
             if node.parent is not None:
                 ret.append(node.edge_data(self.node_holder.idx_use_letter))
